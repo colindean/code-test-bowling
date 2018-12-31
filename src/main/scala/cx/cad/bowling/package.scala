530252f64a08a5a2aba7cd71dcba53008add4604
Colin Dean <git@cad.cx>
2018-12-31 15:37:30 -0500

    Adds a package object to get a better doc

---
+++ src/main/scala/cx/cad/bowling/package.scala
@@ -0,0 +1,15 @@
+package cx.cad
+
+/**
+  * This library provides a way to score games of ten-pin bowling using the
+  * [[https://en.wikipedia.org/wiki/Ten-pin_bowling#Traditional_scoring traditional scoring method]].
+  *
+  * @example
+  *          {{{
+  *          val rolls = StdIn.readLine().split(' ').map(_.toInt)
+  *          val score = Game.from(rolls).score
+  *          }}}
+  */
+package object bowling {
+
+}
